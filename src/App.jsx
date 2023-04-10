874bca020a8532ccf9de2495a655f2608da13f0f
Jijo John <jijojohnlikeu@gmail.com>
2023-04-11 01:06:00 +0530

    added mui containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Box, Container, Typography } from '@mui/material'
 import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Create React App example
+        </Typography>
+      </Box>
+    </Container>
   )
 }
 
