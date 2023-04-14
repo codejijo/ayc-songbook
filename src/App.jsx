0bb03e643964a713c8a7f3566145705e23a1e9ba
Jijo John <jijojohnlikeu@gmail.com>
2023-04-14 17:36:15 +0530

    added firebase to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { Box, Container, Typography } from '@mui/material'
-import { useState } from 'react'
+import { Box, Container, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h3" component="h1" gutterBottom>
           Material UI Create React App example
         </Typography>
       </Box>
